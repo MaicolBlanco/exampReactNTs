d863d0088f93cffae4c6218e4a8bcc8985ee8f77
maicol blanco <m@artcompanysystem.com>
2024-04-13 11:14:28 -0500

    Feat: Connection of the application with Github

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{marginBottom:10}}>Hello Martin Esteban</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "gold",
+          borderRadius: 100,
+        }}
+      ></View>
+      <BoxME bgColor="blue"/>
+      <BoxME bgColor="red" borderColor="#CDCDCD"/>
+      <BoxME bgColor="orange" borderColor="black"/>
     </View>
   );
 }
@@ -13,8 +23,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "olive",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+interface IBoxMe {
+  bgColor: string;
+  borderColor?: 'blue' | 'black' | 'purple' | '#CDCDCD';
+
+}
+
+const BoxME = (props: IBoxMe) => {
+  const {bgColor, borderColor = 'pink'} = props;
+  return (
+    <View
+      style={{
+        marginTop: 10,
+        width: 100,
+        height: 100,
+        backgroundColor: bgColor,
+        borderRadius: 20,
+        borderWidth: 5,
+        borderColor: borderColor,
+      }}
+    ></View>
+  );
+};
+
+/* 
+Para poder usar Typescript en sus proyectos ya hechos, pueden hacer lo siguiente. 1. Ejecutan la siguiente línea en la terminal (estando en su proyecto) {  "extends": "@tsconfig/react-native/tsconfig.json"} `yarn add --dev @tsconfig/react-native @types/jest @types/react @types/react-test-renderer typescript` ```js yarn add --dev @tsconfig/react-native @types/jest @types/react @types/react-test-renderer typescript ```2. Agregan en el directorio raíz el archivo **'tsconfig.json'** y luego ponen lo siguiente```js { "extends": "@tsconfig/react-native/tsconfig.json" } ```
+*/
